84b6d06b8cb637e0bff7bcd00c4d2d5c4163d176
ivalchanka <i.valchenk@hotmail.com>
2023-03-19 22:53:19 +0400

    Implemented into walk scene.

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -5,6 +5,7 @@ public class MoveLeft : MonoBehaviour
     private const float DefaultSpeed = 20;
     private const float MaxSpeed = DefaultSpeed * 2;
 
+    private GameObject playerGameObject;
     private PlayerController playerController;
     private Animator playerAnimator;
     private float currentSpeed = DefaultSpeed;
@@ -12,15 +13,15 @@ public class MoveLeft : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var playerGameObject = GameObject.Find("Player");
-        this.playerController = playerGameObject.GetComponent<PlayerController>();
-        this.playerAnimator = playerGameObject.GetComponent<Animator>();
+        this.playerGameObject = GameObject.Find("Player");
+        this.playerController = this.playerGameObject.GetComponent<PlayerController>();
+        this.playerAnimator = this.playerGameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!this.playerController.gameOver)
+        if (this.CanRun())
         {
             this.currentSpeed = Input.GetKey(KeyCode.W) && this.playerController.isOnGround
                 ? MaxSpeed
@@ -30,9 +31,19 @@ public class MoveLeft : MonoBehaviour
             transform.Translate(Vector3.left * Time.deltaTime * this.currentSpeed);
         }
 
-        if (this.transform.position.x < -15 && this.gameObject.CompareTag("obstacle"))
+        if (this.CanDestroyObstacle())
         {
             Destroy(this.gameObject);
         }
+    }
+
+    private bool CanRun()
+    {
+        return !this.playerController.gameOver && this.playerGameObject.transform.position.x >= PlayerController.PlayerStartPosition;
+    }
+
+    private bool CanDestroyObstacle()
+    {
+        return this.transform.position.x < -15 && this.gameObject.CompareTag("obstacle");
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,9 @@
 public class PlayerController : MonoBehaviour
 {
     private const int MaxNumberOfJumps = 2;
+    private const float WalkSpeed = 1.5f;
+
+    public const float PlayerStartPosition = -2f;
 
     private Rigidbody playerRb;
     private Animator playerAnim;
@@ -42,38 +45,49 @@ public class PlayerController : MonoBehaviour
             this.playerAnim.SetTrigger("Jump_trig");
             this.audioSource.PlayOneShot(this.jumpSound, 1.0f);
         }
+
+        if (this.transform.position.x < PlayerStartPosition)
+        {
+            this.transform.Translate(Vector3.forward * Time.deltaTime * WalkSpeed);
+        }
     }
 
     // Will update the boolean flag once the player touched the ground.
     public void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("ground"))
+        if (this.transform.position.x >= PlayerStartPosition)
         {
-            this.numberOfJumps = 0;
-            this.isOnGround = true;
-            this.dirtParticle.Play();
-        }
-        else if (collision.gameObject.CompareTag("obstacle"))
-        {
-            this.gameOver = true;
-            Debug.Log("Game Over!");
+            if (collision.gameObject.CompareTag("ground"))
+            {
+                this.numberOfJumps = 0;
+                this.isOnGround = true;
+                this.dirtParticle.Play();
+            }
+            else if (collision.gameObject.CompareTag("obstacle"))
+            {
+                this.gameOver = true;
+                Debug.Log("Game Over!");
 
-            this.playerAnim.SetBool("Death_b", true);
-            this.playerAnim.SetInteger("DeathType_int", 1);
+                this.playerAnim.SetBool("Death_b", true);
+                this.playerAnim.SetInteger("DeathType_int", 1);
 
-            this.explosionParticle.Play();
-            this.dirtParticle.Stop();
+                this.explosionParticle.Play();
+                this.dirtParticle.Stop();
 
-            this.audioSource.PlayOneShot(this.crashSound, 1.0f);
-        }
-        else
-        {
-            this.dirtParticle.Stop();
+                this.audioSource.PlayOneShot(this.crashSound, 1.0f);
+            }
+            else
+            {
+                this.dirtParticle.Stop();
+            }
         }
     }
 
     private bool CanDoJump()
     {
-        return Input.GetKeyDown(KeyCode.Space) && (this.isOnGround || this.numberOfJumps < MaxNumberOfJumps) && !this.gameOver;
+        return Input.GetKeyDown(KeyCode.Space)
+            && (this.isOnGround || this.numberOfJumps < MaxNumberOfJumps)
+            && !this.gameOver
+            && this.transform.position.x >= PlayerStartPosition;
     }
 }
